8d87e032dd09c0f5fbaa3aec1f4518ecf46b3eb4
Cody Schwarz <cody.schwarz@gmail.com>
2017-09-16 17:57:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(name='tap-klaviyo',
       version='0.0.1',
-      description='Singer.io tap for extracting data from the GitHub API',
+      description='Singer.io tap for extracting data from the Klaviyo API',
       author='Stitch',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
