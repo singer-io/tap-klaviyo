9a7b7c3e27b4dcf5915f093bc81403967679ab34
Dan Mosora <dan@stitchdata.com>
2018-11-12 16:31:18 +0000

    Version 0.0.6, bump requests to 2.20.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(name='tap-klaviyo',
-      version='0.0.5',
+      version='0.0.6',
       description='Singer.io tap for extracting data from the Klaviyo API',
       author='Stitch',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_klaviyo'],
       install_requires=['singer-python==3.2.1',
-                        'requests==2.13.0'],
+                        'requests==2.20.0'],
       entry_points='''
           [console_scripts]
           tap-klaviyo=tap_klaviyo:main
