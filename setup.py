959d06b524cb7acf4b834cd9e5a0c47821708adf
Jacob Baca <jbaca@talend.com>
2019-09-17 13:45:10 +0000

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-klaviyo',
-      version='0.0.6',
+      version='0.1.0',
       description='Singer.io tap for extracting data from the Klaviyo API',
       author='Stitch',
       url='http://singer.io',
