c7c268a90e470c9864b02fb523c9c6f1eec43076
savan-chovatiya <80703490+savan-chovatiya@users.noreply.github.com>
2021-04-13 21:14:19 +0530

    TDL-684:Allowed fields to be selected via metadata (#25)
    * TDL-684:Allowed fields to be selected via metadata
    
    * TDL-684: Added transform_and_write_records function for similar type of code
    
    * TDL-684: Added empty line at last for utils.py
    
    * TDL-684: Proper new line at last for utils file
    
    * TDL-684: Added timestamp field as automatic inclusion for incremental stream in metadata
    
    Co-authored-by: Savan Chovatiya <savan.chovatiya@CDSYS.LOCAL>

--- tap_klaviyo/__init__.py
+++ tap_klaviyo/__init__.py
@@ -40,20 +40,17 @@ class Stream(object):
 
     def to_catalog_dict(self):
         schema = load_schema(self.stream)
+        mdata = metadata.to_map(
+            metadata.get_standard_metadata(
+                schema = schema,
+                key_properties = self.key_properties,
+                replication_method = self.replication_method
+            )
+        )
 
-        self.metadata.append({
-            'breadcrumb': (),
-            'metadata': {
-                'table-key-properties': self.key_properties,
-                'forced-replication-method': self.replication_method
-            }
-        })
-
-        for k in schema['properties']:
-            self.metadata.append({
-                'breadcrumb': ('properties', k),
-                'metadata': { 'inclusion': 'automatic' }
-            })
+        if self.replication_method == 'INCREMENTAL':
+            mdata = metadata.write(mdata, ('properties', 'timestamp'), 'inclusion', 'automatic')
+        self.metadata = metadata.to_list(mdata)
 
         return {
             'stream': self.stream,
@@ -143,7 +140,7 @@ def discover(api_key):
 
 
 def do_discover(api_key):
-    print(json.dumps(discover(api_key), sys.stdout, indent=2))
+    print(json.dumps(discover(api_key), indent=2))
 
 @singer.utils.handle_top_exception(LOGGER)
 def main():
