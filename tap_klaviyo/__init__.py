905bc0f19301ae18cd444b85b4fcc86209a5b140
shantanu73 <shantanudhiman25@gmail.com>
2024-05-15 03:50:50 +0000

    Changes: 1. Added new APIs for metrics & events streams and removed metric stream which is deprecated. 2. Added API_VERSION which is going to part of headers in Klaviyo API calls. 3. Made code changes to include changes in response of new APIs. 4. Added API_KEY in headers which will be used for authorizing Klaviyo APIs in new implementation.

--- tap_klaviyo/__init__.py
+++ tap_klaviyo/__init__.py
@@ -5,15 +5,17 @@ import os
 import sys
 import singer
 from singer import metadata
-from tap_klaviyo.utils import get_incremental_pull, get_full_pulls, get_all_pages
+from tap_klaviyo.utils import get_incremental_pull, get_full_pulls, get_all_using_next
 
 LOGGER = singer.get_logger()
+
+API_VERSION = "2024-02-15"
 
 ENDPOINTS = {
     'global_exclusions': 'https://a.klaviyo.com/api/v1/people/exclusions',
     'lists': 'https://a.klaviyo.com/api/v1/lists',
-    'metrics': 'https://a.klaviyo.com/api/v1/metrics',
-    'metric': 'https://a.klaviyo.com/api/v1/metric/',
+    'metrics': 'https://a.klaviyo.com/api/metrics',
+    'events': 'https://a.klaviyo.com/api/events',
     'campaigns': 'https://a.klaviyo.com/api/v1/campaigns'
 }
 
@@ -126,8 +128,7 @@ def load_shared_schema_refs():
 
     return shared_schema_refs
 
-def do_sync(config, state, catalog):
-    api_key = config['api_key']
+def do_sync(config, state, catalog, headers):
     start_date = config['start_date'] if 'start_date' in config else None
 
     stream_ids_to_sync = set()
@@ -147,21 +148,21 @@ def do_sync(config, state, catalog):
         )
 
         if stream['stream'] in EVENT_MAPPINGS.values():
-            get_incremental_pull(stream, ENDPOINTS['metric'], state,
-                                 api_key, start_date)
+            get_incremental_pull(stream, ENDPOINTS['events'], state,
+                                 headers, start_date)
         else:
-            get_full_pulls(stream, ENDPOINTS[stream['stream']], api_key)
+            get_full_pulls(stream, ENDPOINTS[stream['stream']], headers)
 
 
-def get_available_metrics(api_key):
+def get_available_metrics(headers):
     metric_streams = []
-    for response in get_all_pages('metric_list',
-                                  ENDPOINTS['metrics'], api_key):
+    for response in get_all_using_next('metric_list',
+                                  ENDPOINTS['metrics'], headers):
         for metric in response.json().get('data'):
-            if metric['name'] in EVENT_MAPPINGS:
+            if metric['attributes']['name'] in EVENT_MAPPINGS:
                 metric_streams.append(
                     Stream(
-                        stream=EVENT_MAPPINGS[metric['name']],
+                        stream=EVENT_MAPPINGS[metric['attributes']['name']],
                         tap_stream_id=metric['id'],
                         key_properties="id",
                         replication_method='INCREMENTAL',
@@ -172,29 +173,32 @@ def get_available_metrics(api_key):
     return metric_streams
 
 
-def discover(api_key):
-    metric_streams = get_available_metrics(api_key)
+def discover(headers):
+    metric_streams = get_available_metrics(headers)
     return {"streams": [a.to_catalog_dict()
                         for a in metric_streams + FULL_STREAMS]}
 
 
-def do_discover(api_key):
-    print(json.dumps(discover(api_key), indent=2))
+def do_discover(headers):
+    print(json.dumps(discover(headers), indent=2))
 
 @singer.utils.handle_top_exception(LOGGER)
 def main():
 
     args = singer.utils.parse_args(REQUIRED_CONFIG_KEYS)
+    headers = {
+        "Authorization": f"Klaviyo-API-Key {args.config.get('api_key')}",
+        "revision": f"{args.config.get('api_version', API_VERSION)}"
+    }
 
     if args.discover:
-        do_discover(args.config['api_key'])
+        do_discover(headers)
 
     else:
-        catalog = args.catalog.to_dict() if args.catalog else discover(
-             args.config['api_key'])
+        catalog = args.catalog.to_dict() if args.catalog else discover(headers)
 
         state = args.state if args.state else {"bookmarks": {}}
-        do_sync(args.config, state, catalog)
+        do_sync(args.config, state, catalog, headers)
 
 if __name__ == '__main__':
     main()
